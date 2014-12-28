6b7f3db11a5483747de29aedaf95956008e138f9
Tadeu Zagallo <tadeuzagallo@gmail.com>
2014-12-28 01:12:41 -0200

    Add sample html + directory organization

---
+++ widget/GithubPulse/ContentViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ContentViewController.swift
+//  GithubPulse
+//
+//  Created by Tadeu Zagallo on 12/28/14.
+//  Copyright (c) 2014 Tadeu Zagallo. All rights reserved.
+//
+
+import Cocoa
+import WebKit
+
+class ContentViewController: NSViewController {
+  override func viewDidLoad() {
+    var indexPath = NSBundle.mainBundle().pathForResource("index", ofType: "html")
+    var url = NSURL(fileURLWithPath: indexPath!)
+    var request = NSURLRequest(URL: url!)
+    
+    var webView:WebView = self.view as WebView
+    webView.drawsBackground = false
+    webView.mainFrame.loadRequest(request)
+    
+    super.viewDidLoad()
+  }
+}
