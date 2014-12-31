a189cc46a8adfb8814bfec2418392e9d38dd0a91
Tadeu Zagallo <tadeuzagallo@gmail.com>
2014-12-31 02:30:08 -0200

    Add podfile for easier startup management

---
+++ widget/GithubPulse/BridgeHeader.h
@@ -0,0 +1,13 @@
+//
+//  BridgeHeader.h
+//  GithubPulse
+//
+//  Created by Tadeu Zagallo on 12/31/14.
+//  Copyright (c) 2014 Tadeu Zagallo. All rights reserved.
+//
+
+#ifndef GithubPulse_BridgeHeader_h
+#define GithubPulse_BridgeHeader_h
+
+
+#endif
