4e260b385b7fc0214a3cae4706b17bb703799b0c
John Knaack <johnknaack@gmail.com>
2015-01-27 20:55:28 -0800

    UserLine now uses classSet

--- javascript/components/UserLine.react.jsx
+++ javascript/components/UserLine.react.jsx
@@ -4,21 +4,27 @@ require('../styles/UserLine')
 
 var UserLine = React.createClass({
   render() {
-    var todayClass = this.props.user.today > 0 ? '' : 'user-line__today-zero';
-    var streakClass = this.props.user.streak > 0 ? '' : 'user-line__bar-streak-zero';
-    var usernameClass = this.props.user.streak > 0 ? '' : 'user-line__username-zero';
+    var cx = React.addons.classSet;
+
+    var hasToday = this.props.user.today > 0;
+    var hasStreak = this.props.user.streak > 0;
+
+    var todayClass = cx({'user-line__today': true, 'user-line__today-zero': !hasToday});
+    var streakClass = cx({'user-line__bar-streak': true, 'user-line__bar-streak-zero': !hasStreak});
+    var usernameClass = cx({'user-line__username': true, 'user-line__username-zero': !hasStreak});
+
     var widthPercent = Math.round((this.props.user.streak / this.props.maxStreak) * 55) + 45;
     if (isNaN(widthPercent)) { widthPercent = 45; }
 
     return (
       <div className="user-line" onClick={ this._profile }>
         <div className="user-line__bar">
-          <div className={ "user-line__bar-streak " + streakClass } style={ { width: widthPercent + '%' } }>
+          <div className={ streakClass } style={ { width: widthPercent + '%' } }>
             <span>{ 'streak ' + (typeof this.props.user.streak !== 'undefined' ? this.props.user.streak : '-') }</span>
           </div>
         </div>
-        <div className={ "user-line__username " + usernameClass }>@{ this.props.user.login }</div>
-        <div className={ "user-line__today " + todayClass }>
+        <div className={ usernameClass }>@{ this.props.user.login }</div>
+        <div className={ todayClass }>
           { typeof this.props.user.today !== 'undefined' ? this.props.user.today : '-' }
         </div>
         <div className="user-line__today-label">today</div>
