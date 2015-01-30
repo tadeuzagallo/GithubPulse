aab804b7bfc062f20cf0b9e3817a205a9d76449c
Tadeu Zagallo <tadeuzagallo@gmail.com>
2015-01-30 16:58:12 -0200

    Remove cookies from chrome requests - prevent load of private data
    Refs #29

--- chrome_extension/js/utils.js
+++ chrome_extension/js/utils.js
@@ -103,6 +103,18 @@ window.Utils = (function () {
     request.send(null);
   };
 
+  chrome.webRequest.onBeforeSendHeaders.addListener(function (details) {
+    for (var i = 0; i < details.requestHeaders.length; ++i) {
+      if (details.requestHeaders[i].name === 'Cookie') {
+        details.requestHeaders.splice(i, 1);
+        break;
+      }
+    }
+    return { requestHeaders: details.requestHeaders };
+  }, {
+    urls: ['https://github.com/users/*/contributions']
+  }, ['blocking', 'requestHeaders']);
+
   Utils.updateIcon = function (commits) {
     var color = commits === 0 ? 'red' : 'blue';
     var imgs = {};
